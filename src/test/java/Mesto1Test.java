d3ae07467683265d5a750df4ccc61a3f0329c5e1
Ekaterina Sysoeva <colnzeee@yandex.ru>
2024-04-11 13:58:26 +0300

    hfhgh

--- src/test/java/Mesto1Test.java
+++ src/test/java/Mesto1Test.java
@@ -1,9 +1,9 @@
 import io.qameta.allure.Step;
+import io.qameta.allure.junit4.DisplayName;
 import io.restassured.RestAssured;
 import jdk.jfr.Description;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.DisplayName;
 
 import static io.restassured.RestAssured.given;
 
